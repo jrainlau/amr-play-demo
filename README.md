4f307e4a45ec62e2fc42e282adb65cad12508f22
jrainliu <jrainliu@tencent.com>
2021-03-15 11:10:04 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,18 @@
+# AMR 音频文件播放 DEMO
+
+首先请全局安装一个静态资源服务器，如 [http-server](https://www.npmjs.com/package/http-server) 或者 [vite](https://vitejs.dev/guide/)（此处推荐 `vite`，它支持热更新，本地修改的内容会实时推送到浏览器）。
+
+- http-server:
+  ```bash
+  sudo yarn add http-server -g
+
+  http-server
+  ```
+
+- vite:
+  ```bash
+  sudo yarn add vite -g
+
+  vite
+  ```
+
---
+++ main.js
@@ -0,0 +1,61 @@
+import AMR from "./amr.js"
+
+function fetchBlob(url) {
+  return fetch(url).then((res) => res.blob())
+}
+
+function readBlob(blob) {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+    reader.onload = function (e) {
+      const data = new Uint8Array(e.target.result)
+      resolve(data)
+    }
+    reader.readAsArrayBuffer(blob)
+  })
+}
+
+function playAmrArray(array) {
+  const samples = AMR.decode(array)
+  if (!samples) {
+    alert("Failed to decode!")
+    return
+  }
+  playPcm(samples)
+}
+
+let gAudioContext = new AudioContext()
+
+function getAudioContext() {
+  if (!gAudioContext) {
+    gAudioContext = new AudioContext()
+  }
+  return gAudioContext
+}
+function playPcm(samples) {
+  const ctx = getAudioContext()
+  const src = ctx.createBufferSource()
+  const buffer = ctx.createBuffer(1, samples.length, 8000)
+  if (buffer.copyToChannel) {
+    buffer.copyToChannel(samples, 0, 0)
+  } else {
+    const channelBuffer = buffer.getChannelData(0)
+    channelBuffer.set(samples)
+  }
+
+  src.buffer = buffer
+  src.connect(ctx.destination)
+  src.start()
+}
+
+async function playAmrBlob(blob) {
+  const data = await readBlob(blob)
+  playAmrArray(data)
+}
+
+async function playAmr(url) {
+  const blob = await fetchBlob(url)
+  playAmrBlob(blob)
+}
+
+export default playAmr
